fb1514cf20690b4dfbe034c8dc9ae970981b9a06
Kashina69 <princerawatji69@gmail.com>
2024-10-23 18:53:15 +0530

    FIX: ts_ls but not clangd

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "eslint", "gopls", "rust_analyzer", "marksman" } -- Added servers for TypeScript, ESLint, C, Go, Rust, and Markdown
+local servers = { "html", "cssls","ts_ls","clangd", "eslint", "gopls", "rust_analyzer", "marksman" } -- Added servers for TypeScript, ESLint, C, Go, Rust, and Markdown
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,9 +16,3 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
